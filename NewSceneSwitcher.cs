be2a0af4f0c301a43ab3f5fd372dc4050128179b
Joshua Reynolds <71860187+Joshua-Reynolds-MTU@users.noreply.github.com>
2022-01-28 18:17:40 -0500

    Update NewSceneSwitcher.cs

--- NewSceneSwitcher.cs
+++ NewSceneSwitcher.cs
@@ -1,5 +1,31 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI; using UnityEngine.SceneManagement;  public class NewSceneSwitcher : MonoBehaviour {      public void Awake()     {         DontDestroyOnLoad(this);     }      public void starter()     {         SceneManager.LoadScene(sceneBuildIndex: 4);     }      public void invokeSwitcher()
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewSceneSwitcher : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void starter()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 4);
+    }
+
+    public void invokeSwitcher()
     {
         int index = Random.Range(1, 4);
         SceneManager.LoadScene(index);
-    }      public void quitGame()     {         Debug.Log("Quitting game...");         Application.Quit();     } }
+    }
+    
+    public void quitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }    
+}
