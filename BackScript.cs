638f4de64292f71d955018bf6d82503a24ef5c9c
Joshua Reynolds <71860187+Joshua-Reynolds-MTU@users.noreply.github.com>
2022-01-28 18:14:12 -0500

    Update BackScript.cs

--- BackScript.cs
+++ BackScript.cs
@@ -1,1 +1,13 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.SceneManagement;  public class BackScript : MonoBehaviour {     public void goBack()     {         SceneManager.LoadScene("Menu");     } }  
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackScript : MonoBehaviour
+{
+  public void goBack()
+  {
+    SceneManager.LoadScene("Menu");
+  }
+}
+    
