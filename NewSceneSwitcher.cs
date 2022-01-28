dd2f88ba69fdc5fb1539200c9931572855fbc459
Joshua Reynolds <71860187+Joshua-Reynolds-MTU@users.noreply.github.com>
2022-01-28 18:12:53 -0500

    Add files via upload

---
+++ NewSceneSwitcher.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI; using UnityEngine.SceneManagement;  public class NewSceneSwitcher : MonoBehaviour {      public void Awake()     {         DontDestroyOnLoad(this);     }      public void starter()     {         SceneManager.LoadScene(sceneBuildIndex: 4);     }      public void invokeSwitcher()
+    {
+        int index = Random.Range(1, 4);
+        SceneManager.LoadScene(index);
+    }      public void quitGame()     {         Debug.Log("Quitting game...");         Application.Quit();     } }
